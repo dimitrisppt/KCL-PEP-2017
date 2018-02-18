6dbf806d52a396132cc60cc0c2a1bb2217bcc236
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2018-02-18 17:48:48 +0000

    Implemented deallocation

--- heap.h
+++ heap.h
@@ -92,23 +92,7 @@ public:
     char * allocateMemoryWorstFit(size_t requested) {
         int rounded_requested = (requested / 4) * 4;
 
-        MemControlBlock * curr = startOfHeap;
-        MemControlBlock * worstFit = nullptr;
-
-        while (curr != nullptr) {
-            if (curr->available) {
-                if (curr->size >= rounded_requested) {
-                    if (worstFit != nullptr) {
-                        if (curr->size < worstFit->size) {
-                            worstFit = curr;
-                        }
-                    } else {
-                        worstFit = curr;
-                    }
-                }
-            }
-            curr = curr->next;
-        }
+        MemControlBlock * worstFit = worstFitAlg(rounded_requested);
 
         if (worstFit != nullptr) {
             worstFit->available = false;
@@ -127,9 +111,28 @@ public:
             char * startAddress = sizeof(MemControlBlock) + worstFitPos;
             return startAddress;
         }
-        
+
         return nullptr;
 
+    }
+
+    MemControlBlock * worstFitAlg(int rounded_requested) {
+        MemControlBlock * curr = startOfHeap;
+        MemControlBlock * worstFit = nullptr;
+        do {
+            if (curr->available) {
+                if ((worstFit == nullptr) && (curr->size >= rounded_requested)) {
+                       worstFit = curr;
+                } else if (!(worstFit == nullptr) && (curr->size >= rounded_requested)) {
+                    if (curr->size < worstFit->size) {
+                        worstFit = curr;
+                    }
+                }
+            }
+            curr = curr->next;
+
+        } while (curr != nullptr);
+        return worstFit;
     }
 
 
@@ -140,9 +143,33 @@ public:
 
     /** @brief Deallocate the memory used by the object at the given address */
     void deallocateMemory(char * toDeallocate) {
-        // TODO: your code for deallocateMemory memory goes here
+        char * address = toDeallocate - sizeof(MemControlBlock);
+        MemControlBlock * block = reinterpret_cast<MemControlBlock *>(address);
+        block->available = true;
+
+        cout << block->previous << endl;
+
+        if (block->previous) {
+            if (block->previous->available) {
+
+                block->previous->size = block->previous->size + block->size + sizeof(MemControlBlock);
+                block->previous = block->next;
+                //block->next = block->previous;
+
+            }
+        }
+
+         if (block->next) {
+             if (block->next->available) {
+
+                block->next->size = block->next->size + block->size + sizeof(MemControlBlock);
+                block->next = block->previous;
+                //block->previous = block->next;
 
 
+            }
+
+        }
 
     }
 };
